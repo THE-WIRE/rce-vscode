5b3bf81690fc160a974c9201235455c8ecb8981d
Suyog Gadhave <itssuyog96@gmail.com>
2018-01-12 21:29:27 +0530

    initiated revolution

--- extension.js
+++ extension.js
@@ -1,55 +1,124 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
-var btoa = require("btoa");
-var io = require('socket.io-client');
-var socket = io.connect('https://rce-server.herokuapp.com/');
+const vscode = require('vscode');
+const btoa = require("btoa");
+const EventHandler = require('./event_handler');
+const VSCodeShare = require('./vscode_share');
+const WebSocket = require("ws");
+const NewSessionView = require('./new-session-view');
+const SessionView = require('./session-view');
+const RemoteCursorView = require("./remote-cursor-view");
 
+const config = {
+    version: require('./package.json').version,
+    serverAddress: 'wire-pair-server.herokuapp.com',
+    serverPort : 80,
+    secureConnection : false,
+    userEmail: '',
+    syncTabs : true
+}
 
-socket.on('init', function(data){
-    console.log("Connected to server")
-})
+function setDefaultValues(){
+    this.address = config.serverAddress;
+    this.portNumber = config.serverPort;
+    this.secureConnection = config.secureConnection;
+}
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function createSocketConnection(){
+    setDefaultValues();
+
+    const proto = this.secureConnection ? 'wss' : 'ws';
+
+    return new WebSocket(`${proto}://${this.address}:${this.portNumber}`);
+}
+
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "rce" is now active!');
+    var connectDisposable = vscode.commands.registerCommand('extension.rce-connect', () => startSession());
+    var disconnectDisposable = vscode.commands.registerCommand('extension.rce-disconnect', () => deactivate());
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+    context.subscriptions.push(connectDisposable);
+    context.subscriptions.push(disconnectDisposable);
+}
 
-        socket.on('init', function (data) {
-            console.log(data);
-          });
+function startSession(){
 
-          vscode.window.showInputBox().then(function (username) {
-            vscode.window.showInputBox().then(function (password) {
+    this.sessionStatus = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    this.sessionStatus.text = "$(broadcast)RCE : Enter team Key...";
+    this.sessionStatus.show();
+    
+    vscode.window.showInputBox({ prompt: "Enter Team Key", ignoreFocusOut: true})
+    .then(sessionId => {
+        this.sessionStatus.text = "$(broadcast)RCE : Initiating connection...";
+        // sessionId == ''? vscode.window.showErrorMessage("Session Id Cannot be empty!") : connect(sessionId);
+    })
+}
 
-                
-                socket.emit('creds', {
-                    username : username,
-                    password: btoa(password)
-                })
-
-                socket.on('confirm', function(data){
-                    vscode.window.showInformationMessage(data.p);
-                })
+function setupHeartbeat(){
+    this.hearbeatId = setInterval(() => {
+        try {
+            this.ws.send('ping', error => {
+                if(error != null){
+                    this.event_handler.emitter.emit('socket-not-opened');
+                }
             })
-        })
-        
+        } catch (error1){
+            this.event_handler.emitter.emit('socket-not-opened');
+            clearInterval(this.hearbeatId);
+        }
+    }, 30000);
+}
+
+function connect(sessionId){
+    this.ws = this.ws == null ? createSocketConnection() : this.ws;
+
+    this.ws.on("open",  () => {
+        vscode.window.showInformationMessage("Session Started");
+        setupHeartbeat();
+        this.vscode_share = new VSCodeShare(this.ws);
+        this.vscode_share.start(sessionId);
+
+        this.event_handler = new EventHandler(this.ws);
+        this.event_handler.listen();
+
+        this.event_handler.emitter.on('socket-not-opened', () => {
+            vscode.window.showWarningMessage("RCE : Connection Lost");
+            this.deactivate();
+        });
+
+        //TODO : this.sessionStatusView = new SessionView;
+
     });
 
-    context.subscriptions.push(disposable);
+    this.ws.on('error',  e => {
+        console.log('error', e);
+        vscode.window.showErrorMessage('RCE : Could not connect');
+        this.deactivate();
+    })
 }
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {
+    if(this.heartbeatId){
+        clearInterval(this.heartbeatId);
+    }
+    if(this.ws != null){
+        this.sessionStatus.hide();
+        vscode.window.showInformationMessage('RCE : Disconnected');
+        this.ws.close();
+        this.ws = null;
+        if(this.event_handler != null){
+            this.event_handler.subscriptions.dispose();
+        }
+        if(this.vscode_share != null){
+            this.vscode_share.subscriptions.dispose();
+        }
+        
+    }
+    else{
+        vscode.showWarningMessage("RCE : No active sessions found");
+    }
+    
 }
 exports.deactivate = deactivate;
+exports.config = config
